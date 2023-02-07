dae830987598fe743f8edb59ae675bbb88ff14ed
ATantasheva <96054339+ATantasheva@users.noreply.github.com>
2023-02-07 09:43:09 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,20 +1,20 @@
 
 module.exports = function towelSort (matrix) {
-	let arr = [];
+	let arr1 = [];
 
-	if (!Array.isArray(matrix)) return arr;
+	if (!Array.isArray(matrix)) return arr1;
 
 	for (let i = 0; i < matrix.length; i++) {
 		if (i % 2 === 0) {
 			for (let j = 0; j < matrix[i].length; j++) {
-				arr.push(matrix[i][j]);
+				arr1.push(matrix[i][j]);
 			}
 		} else {
-			for (let j = matrix[i].length - 1; j >= 0; j--) {
-				arr.push(matrix[i][j]);
+			for (let num = matrix[i].length - 1; num >= 0; num--) {
+				arr1.push(matrix[i][num]);
 			}
 		}
 
 	}
-  return arr;
+  return arr1;
 }
